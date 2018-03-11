01e206289a0f2f4b2dbd00e461845489d6f8bb87
isao_e <isao0214@gmail.com>
2018-03-11 17:26:29 +0900

    ~/pages/index.vueにて、isMobileを読み込み

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,22 @@
     <h1 class="title">
       portfolioだ
     </h1>
+    <div v-if="isMobile">
+      <p>app.vueスマートフォン</p>
+    </div>
+    <div v-else>
+      <p>app.vueパソコン</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'app',
+  asyncData({ isMobile }) {
+    return {
+      isMobile,
+    }
+  }
 };
 </script>
