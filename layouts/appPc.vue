295ca3920c52a30b0b7294a7ea347554eecd054f
isao_e <isao0214@gmail.com>
2018-03-17 14:50:39 +0900

    layouts/appPc.vueを作成

---
+++ layouts/appPc.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <header-pc/>
+    <nuxt/>
+    <footer-pc/>
+  </div>
+</template>
+
+<script>
+import HeaderPc from '~/components/common/HeaderPc.vue';
+import FooterPc from '~/components/common/FooterPc.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HeaderPc,
+    FooterPc,
+  },
+};
+</script>
