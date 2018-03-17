9cb094b57da55ef7e5afb6e78975201acb5c3cbb
isao_e <isao0214@gmail.com>
2018-03-17 14:50:06 +0900

    pages/index.vueにて、header, footerの読み込みを削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,21 @@
 <template>
   <div>
     <template v-if="isMobile">
-      <header-sp/>
       <index-sp/>
-      <footer-sp/>
     </template>
     <template v-else>
-      <header-pc/>
       <index-pc/>
-      <footer-pc/>
     </template>
   </div>
 </template>
 
 <script>
-import HeaderSp from '~/components/common/HeaderSp.vue';
-import HeaderPc from '~/components/common/HeaderPc.vue';
-import FooterSp from '~/components/common/FooterSp.vue';
-import FooterPc from '~/components/common/FooterPc.vue';
 import IndexSp from '~/components/IndexSp.vue';
 import IndexPc from '~/components/IndexPc.vue';
 
 export default {
   layout: 'app',
   components: {
-    HeaderSp,
-    HeaderPc,
-    FooterSp,
-    FooterPc,
     IndexSp,
     IndexPc,
   },
