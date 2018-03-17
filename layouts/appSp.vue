8ff7b7b7577192af547db50c361f6f1b17629293
isao_e <isao0214@gmail.com>
2018-03-17 14:50:53 +0900

    layouts/appSp.vueを作成

---
+++ layouts/appSp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <header-sp/>
+    <nuxt/>
+    <footer-sp/>
+  </div>
+</template>
+
+<script>
+import FooterSp from '~/components/common/FooterSp.vue';
+import HeaderSp from '~/components/common/HeaderSp.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HeaderSp,
+    FooterSp,
+  },
+};
+</script>
